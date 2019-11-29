fd345e620fea8bf093f3bc6293637eda92f2a579
alireza <alirezarezvanifakhr@gmail.com>
2019-11-29 13:51:31 +0330

    2nd Commit

--- src/com/team3d/Main.java
+++ src/com/team3d/Main.java
@@ -3,6 +3,34 @@ package com.team3d;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("First Night!");
+
+
     }
 }
+
+
+interface Votable{
+
+}
+
+class Person{
+    boolean isAlive = true;
+    boolean isMafia;
+    String talk;
+
+}
+
+    class Citizen extends Person{}
+
+        class Doctor extends Citizen{}
+        class Sniper extends Citizen{}
+        class Detective extends Citizen{}
+
+    class Mafia extends Person{}
+
+        class  GodFather extends Mafia{}
+        class Natasha extends Mafia{}
+
+
